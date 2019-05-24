a6d517f5b37a655c02c6b42e32866a2b0d0d671a
lalogicaweb <e.valenzuelap@alumnos.duoc.cl>
2019-05-24 18:45:19 -0400

    creacion primer componente marca

--- front-web-formularios-cooperativa/src/app/app.module.ts
+++ front-web-formularios-cooperativa/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcasComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-web-formularios-cooperativa/src/app/components/marcas/marcas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marcas',
+  templateUrl: './marcas.component.html',
+  styles: []
+})
+export class MarcasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
